16cc4d7b86da09d152775e50886bdb554d7efb50
supermistral <53310165+supermistral@users.noreply.github.com>
2020-09-05 11:56:57 +0300

    Update work1.cpp

--- work1.cpp
+++ work1.cpp
@@ -1,4 +1,4 @@
-/ temp1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// temp1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <iostream>
@@ -39,7 +39,7 @@ void num4() {
 void num5() {
     // char x;
     bool street, curtains, lamp;
-    cout << "На улице день? True/False" << endl;
+    cout << "На улице день? 1/0" << endl;
     cin >> street;
     cout << "Шторы раздвинуты?" << endl;
     cin >> curtains;
@@ -51,8 +51,9 @@ void num5() {
 
 void num6() {
     float h, R, r, l, V, S;
-    cout << "Ввод h, R, r, l" << endl;
-    cin >> h >> R >> r >> l;
+    cout << "Ввод h, R, r" << endl;
+    cin >> h >> R >> r;
+    l = sqrt(h * h + (R - r) * (R - r));
     V = 1 / 3 * M_PI * h * (R * R + R * r + r * r);
     S = M_PI * (R * R + l * (R + r) + r * r);
     cout << V << endl << S;
@@ -60,7 +61,7 @@ void num6() {
 
 void num7() {
     float a, x, res;
-    cout << "Ввод a, x";
+    cout << "Ввод a, x" << endl;
     cin >> a >> x;
     x = abs(x);
     if (x < 1) res = sqrt(a * a - x * x);
@@ -71,12 +72,76 @@ void num7() {
 
 }
 
+void num8() {
+    float x, y, b;
+    cout << "Ввод x, y, b" << endl;
+    cin >> x >> y >> b;
+    cout << log(b - y) * sqrt(b - x);
+}
+
+void num9() {
+    int n;
+    cout << "Ввод N" << endl;
+    cin >> n;
+    for (int i = 0; i < 10; i++) {
+        cout << n + i << endl;
+    }
+}
+
+void num10_helper(float x) {
+    if (x - 1 != 0) {
+        cout << x * x - 2 * x + 2 / (x - 1) << endl;
+    }
+    else cout << "Невозможно вычислить" << endl;
+}
+
+void num10() {
+    float x = 4;
+    for (float i = -4; i <= 4; i += 0.5) {
+        cout << i << ") ";
+        num10_helper(i);
+    }
+}
+
+double num11_helper(float S, float p, int n) {
+    float r = p / 100;
+    float temp = pow(1 + r, n);
+    return S * r * temp / (12 * (temp - 1));
+}
+
+void num11() {
+    float S, p;
+    int n;
+    cout << "Ввод S, p, n" << endl;
+    cin >> S >> p >> n;
+    cout << num11_helper(S, p, n) << endl;
+}
+
+void num12() {
+    float S, n;
+    float p = 0;
+    double m, m_temp;
+    cout << "Ввод S, m, n" << endl;
+    cin >> S >> m >> n;
+    for (float i = 0; i <= 100; i += 0.01) {
+        m_temp = num11_helper(S, i, n);
+        if (m_temp == m) {
+            p = i * 100;
+            break;
+        }
+    }
+    if (p) cout << p;
+    else cout << "Не удалось определить p";
+}
+
 int main()
 {
     setlocale(LC_ALL, "rus");
     //num3();
     //num4();
-    num5();
+    //num9();
+    //num10();
+    num12();
     return 0;
 }
 
