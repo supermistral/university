ca51f2720b2171f41d782ffb13908e708e0d8305
Michael <53310165+supermistral@users.noreply.github.com>
2020-10-01 20:57:18 +0300

    Create sinx.cpp

---
+++ sinx.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+double f(double x) {
+    cout << x << " " << sin(x) << endl;
+    return sin(x);
+}
+
+int main()
+{
+    const int n = 27, m = 101, half_n = n / 2, half_m = m / 2;
+    char coords[n][m];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (j == half_m) {
+                coords[i][j] = '|';
+            }
+            else if (i == half_n) {
+                coords[i][j] = '-';
+            }
+            else {
+                coords[i][j] = ' ';
+            }
+        }
+    }
+
+    int posy;
+    for (int i = -half_m; i < half_m; i++) {
+        posy = round(half_n - half_n * f(i / 7.0));
+        coords[posy][i + half_m] = '*';
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << coords[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
