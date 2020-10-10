6dc726977c51b5b51721e51a5bf372fb67692748
Michael <53310165+supermistral@users.noreply.github.com>
2020-10-10 11:20:51 +0300

    Update dz4.cpp

--- dz4.cpp
+++ dz4.cpp
@@ -94,7 +94,45 @@ void task4() {
     }
 }
 
+double f(double x) {
+    cout << x << " " << sin(x) << endl;
+    return sin(x);
+}
+
 void task5() {
+    const int n = 21, m = 61, half_n = n / 2, half_m = m / 2;
+    double scale = M_PI / 14;
+    char coords[n][m];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (j == half_m) {
+                coords[i][j] = '|';
+            }
+            else if (i == half_n) {
+                coords[i][j] = '-';
+            }
+            else {
+                coords[i][j] = ' ';
+            }
+        }
+    }
+
+    int posy;
+    for (int i = -half_m; i < half_m; i++) {
+        posy = round(half_n - half_n * f(i * scale));
+        coords[posy][i + half_m] = '*';
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << coords[i][j];
+        }
+        cout << endl;
+    }
+}
+
+/*void task5() {
     int startPos = 100, scaleX = 10, scaleY = -20;
     HDC window = GetDC(GetConsoleWindow());
     HPEN obj = CreatePen(PS_SOLID, 2, RGB(255, 255, 255));
@@ -109,7 +147,7 @@ void task5() {
         MoveToEx(window, x * scaleX + startPos, sin(x) * scaleY + startPos, NULL);
         LineTo(window, x * scaleX + startPos, sin(x) * scaleY + startPos);
     }
-}
+}*/
 
 void task6() {
     map <char, int> dig = {
