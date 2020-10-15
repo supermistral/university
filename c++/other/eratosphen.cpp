b3fc29e3fcbee9b15a83c12080984caa223112a4
Michael <53310165+supermistral@users.noreply.github.com>
2020-10-15 23:59:48 +0300

    Create eratosphen.cpp

---
+++ c++/other/eratosphen.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void eratosphen(int x) {
+    x++;
+    int* arr = new int[x];
+    for (int i = 0; i < x; i++) {
+        arr[i] = i;
+    }
+
+    for (int i = 2; i < x; i++) {
+        if (arr[i]) {
+            for (int j = arr[i] * arr[i]; j < x; j += arr[i]) {
+                arr[j] = 0;
+            }
+        }
+    }
+
+    for (int i = 2; i < x; i++) {
+        if (arr[i]) cout << arr[i] << endl;
+    }
+    delete[] arr;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int border;
+    cout << "Введите границу -> "; cin >> border;
+    eratosphen(border);
+
+    return 0;
+}
