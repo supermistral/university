af5f478ef222307b0de061e574bc3a69f2b54baf
Michael <53310165+supermistral@users.noreply.github.com>
2020-10-16 00:04:01 +0300

    Create evklid.cpp

---
+++ c++/other/evklid.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int evklid(int x, int y) {
+    while (x != 0 && y != 0) {
+        if (x > y) {
+            x %= y;
+        }
+        else {
+            y %= x;
+        }
+    }
+    return x + y;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int x, y;
+    cout << "Ввод 1-ого числа -> "; cin >> x;
+    cout << "Ввод 2-ого числа -> "; cin >> y;
+    cout << "НОД = " << evklid(x, y);
+
+    return 0;
+}
