2da1ad0adab23040ba1d4d9a852bbbdb849ef9cf
Michael <53310165+supermistral@users.noreply.github.com>
2020-10-15 23:56:01 +0300

    Create balls.cpp

---
+++ c++/other/balls.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void swap(int* arr, int i, int j) {
+    int k = arr[i];
+    arr[i] = arr[j];
+    arr[j] = k;
+}
+
+bool check(int* arr, int n) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == i + 1) return true;
+    }
+    return false;
+}
+
+int perm(int* arr, int n, int start) {
+    int sum = 0;
+    if (start == n) {
+        if (check(arr, n)) {
+            return 1;
+        }
+    }
+    else {
+        for (int i = start; i < n; i++) {
+            swap(arr, i, start);
+            sum += perm(arr, n, start + 1);
+            swap(arr, i, start);
+        }
+    }
+    return sum;
+}
+
+int main()
+{
+    int n;
+    cout << "Введите n -> "; cin >> n;
+    int* a = new int[n];
+    for (int i = 0; i < n; i++) {
+        a[i] = i + 1;
+    }
+    cout << perm(a, n, 0);
+
+    return 0;
+}
