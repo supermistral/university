e22f105330d0d3ee7ed682c7157bb898d9f55d30
Michael <53310165+supermistral@users.noreply.github.com>
2020-10-31 10:22:55 +0300

    Update evklid.cpp

--- c++/other/evklid.cpp
+++ c++/other/evklid.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int evklid(int x, int y) {
+int gcd1(int x, int y) {
     while (x != 0 && y != 0) {
         if (x > y) {
             x %= y;
@@ -14,14 +14,53 @@ int evklid(int x, int y) {
     return x + y;
 }
 
+int gcd2(int x, int y) {
+    while (x != y) {
+        if (x > y) {
+            x -= y;
+        }
+        else {
+            y -= x;
+        }
+    }
+    return x;
+}
+
+void handler(int x, int y) {
+    char input;
+    cout << "Умножением - 1, вычитанием - 2 -> "; cin >> input;
+    switch (input) {
+        case '1':
+            cout << "НОД = " << gcd1(x, y);
+        case '2':
+            cout << "НОД = " << gcd2(x, y);
+        default:
+            return false;
+    }
+    return true;
+}
+
+bool check(int x, int y) {
+    if (int(x) == x && int(y) == y) {
+        return true;
+    }
+    return false;
+}
+    
+
 int main()
 {
     setlocale(LC_ALL, "rus");
 
     int x, y;
-    cout << "Ввод 1-ого числа -> "; cin >> x;
-    cout << "Ввод 2-ого числа -> "; cin >> y;
-    cout << "НОД = " << evklid(abs(x), abs(y));
+    bool running = true;
+    while (running) {
+        cout << "Ввод 1-ого числа -> "; cin >> x;
+        cout << "Ввод 2-ого числа -> "; cin >> y;
+        if (check(x, y)) {
+            running = handler(abs(x), abs(y));
+        }
+    }
 
     return 0;
 }
