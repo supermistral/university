55c776d0002ff17e9b9f6fdb0a1fa82bd71a645c
Michael <53310165+supermistral@users.noreply.github.com>
2020-10-31 10:29:10 +0300

    Update evklid.cpp

--- c++/other/evklid.cpp
+++ c++/other/evklid.cpp
@@ -26,40 +26,46 @@ int gcd2(int x, int y) {
     return x;
 }
 
-void handler(int x, int y) {
+bool handler(int x, int y) {
     char input;
     cout << "Умножением - 1, вычитанием - 2 -> "; cin >> input;
     switch (input) {
-        case '1':
-            cout << "НОД = " << gcd1(x, y);
-        case '2':
-            cout << "НОД = " << gcd2(x, y);
-        default:
-            return false;
+    case '1':
+        cout << "НОД = " << gcd1(x, y);
+        break;
+    case '2':
+        cout << "НОД = " << gcd2(x, y);
+        break;
+    default:
+        return false;
     }
     return true;
 }
 
-bool check(int x, int y) {
+bool check(float x, float y) {
     if (int(x) == x && int(y) == y) {
         return true;
     }
     return false;
 }
-    
+
 
 int main()
 {
     setlocale(LC_ALL, "rus");
 
-    int x, y;
+    float x, y;
     bool running = true;
     while (running) {
         cout << "Ввод 1-ого числа -> "; cin >> x;
         cout << "Ввод 2-ого числа -> "; cin >> y;
         if (check(x, y)) {
-            running = handler(abs(x), abs(y));
+            running = handler(abs(int(x)), abs(int(y)));
         }
+        else {
+            cout << "Числа должны быть целыми";
+        }
+        cout << "\n\n";
     }
 
     return 0;
