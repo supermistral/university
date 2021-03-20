a3c6b5f78c81a370097f3f8e08c8254ae1f5d5c6
Michael <53310165+supermistral@users.noreply.github.com>
2021-03-20 22:19:16 +0300

    Update practice3.cpp

--- siaod/practice3.cpp
+++ siaod/practice3.cpp
@@ -10,7 +10,7 @@ void print_array(int*, int);
 void fill_random_array(int*, int, int = 10);
 void fill_sorted(int*, int, char);
 void insertion_sort(int*, int);
-void swap(int*, int, int);
+void swap(int&, int&);
 
 int main()
 {
@@ -23,22 +23,25 @@ int main()
 	std::cout << "Max = "; std::cin >> max;
 	int* arr = new int[length];
 
+	//fill_sorted(arr, length, '+');
 	fill_random_array(arr, length, max);
-
-	auto begin = std::chrono::steady_clock::now();
-	exchange_sort(arr, length);
-	auto end = std::chrono::steady_clock::now();
-	auto total = std::chrono::duration_cast<std::chrono::seconds>(end - begin);
+	//print_array(arr, length);
+	//auto begin = std::chrono::steady_clock::now();
+	insertion_sort(arr, length);
+	//print_array(arr, length);
+	//auto end = std::chrono::steady_clock::now();
+	//auto total = std::chrono::duration_cast<std::chrono::seconds>(end - begin);
 	//std::cout << total.count() << " s";
 
 	delete[] arr;
 	return 0;
 }
 
-void swap(int* arr, int i, int j) {
-	int k = arr[i];
-	arr[i] = arr[j];
-	arr[j] = k;
+//void swap(int* arr, int i, int j) {
+void swap(int &elem1, int &elem2) {
+	int k = elem1;
+	elem1 = elem2;
+	elem2 = k;
 }
 
 void selection_sort(int* arr, int n) {
@@ -48,25 +51,36 @@ void selection_sort(int* arr, int n) {
 		for (int j = i + 1; j < n; ++j)
 			if (arr[j] < arr[min])
 				min = j;
-		swap(arr, min, i);
+		swap(arr[min], arr[i]);
 	}
 }
 
 void exchange_sort(int* arr, int n) {
+	//long long comps = 0, moves = 0;
 	for (int i = 0; i < n - 1; ++i)
-		for (int j = 0; j < n - i - 1; ++j)
-			if (arr[j] > arr[j + 1])
-				swap(arr, j, j + 1);
+		for (int j = 0; j < n - i - 1; ++j) {
+			//++comps;
+			if (arr[j] > arr[j + 1]) {
+				//++moves;
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	//std::cout << "Comps: " << comps << "\nMoves: " << moves;
 }
 
 void insertion_sort(int* arr, int n) {
+	long long comps = 0, moves = 0;
 	int x, j;
 	for (int i = 1; i < n; ++i) {
 		x = arr[i];
-		for (j = i - 1; j >= 0 && arr[j] > x; --j)
+		for (j = i - 1; j >= 0 && ++comps && arr[j] > x; --j) {
+			++moves;
 			arr[j + 1] = arr[j];
+		}
+		++moves;
 		arr[j + 1] = x;
 	}
+	std::cout << "Comps: " << comps << "\nMoves: " << moves;
 }
 
 void fill_array(int* arr, int n) {
