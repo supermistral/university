fce539df0a1eac93714209a0a60e4fced7916887
Michael <53310165+supermistral@users.noreply.github.com>
2021-04-23 21:11:38 +0300

    Update practice5_ver3.cpp

--- siaod/practice5_ver3.cpp
+++ siaod/practice5_ver3.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <ctime>
 #include <vector>
+#include <chrono>
 
 using std::string;
 using std::ofstream;
@@ -10,14 +11,15 @@ using std::ifstream;
 
 class FileHandler {
 	string fileInput, fileOutput1, fileOutput2;
-	int length = 0, requiredNumber = 0;
+	int length = 0, requiredNumber = 4;
 	std::vector<std::vector<string>> fileFields;
+
 
 public:
 	FileHandler(string, string, string,
 		std::vector<std::vector<string>>);
 	void division(int);
-	void merge(int, int, int);
+	void merge(int);
 	void runSort();
 	string getField(string);
 	bool compareLines(string, string);
@@ -28,12 +30,59 @@ public:
 	bool checkFiles();
 	void printFile();
 
+	void naturalFirstDivision();
 	void naturalDivision();
-	void naturalMerge();
+	void naturalMerge(int&);
+	void runNaturalSort();
 
 	template<typename T1, typename T2, typename T3>
 	void closeFiles(T1&, T2&, T3&);
+
+	int moves = 0, comps = 0;
 };
+
+
+
+int main() {
+	string fileInput	= "D:\\forwork\\siaod5\\input.txt",
+		fileOutput1		= "D:\\forwork\\siaod5\\output1.txt",
+		fileOutput2		= "D:\\forwork\\siaod5\\output2.txt";
+
+	std::vector<std::vector<string>> fields = {
+		{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"}, 
+		{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"},
+		{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"},
+		{"18-70"},
+		{"150-210"},
+		{"green", "blue", "black", "brown"},
+		{"black", "blond", "brown", "grey"},
+		{"1000-1000000"},
+		{"yes", "no"},
+		{"yes", "no"}
+	};
+
+	FileHandler filehandler(fileInput, fileOutput1, fileOutput2, fields);
+	if (!filehandler.checkFiles())
+		return -1;
+	filehandler.generateData();
+	//auto begin = std::chrono::steady_clock::now();
+	filehandler.runNaturalSort();
+	//int k = 0;
+	//filehandler.naturalDivision(k);
+	//filehandler.naturalMerge();
+	//int k = 0;
+	//filehandler.naturalFirstDivision(k);
+	//filehandler.naturalMerge();
+	//filehandler.naturalDivision(k);
+	//filehandler.runSort();
+	//auto end = std::chrono::steady_clock::now();
+	//auto total = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+	//std::cout << total.count() << " ms";
+	std::cout << "Comps: " << filehandler.comps << "\nMoves: " << filehandler.moves
+		<< "\nTotal: " << filehandler.comps + filehandler.moves;
+	return 0;
+}
+
 
 
 FileHandler::FileHandler(
@@ -51,7 +100,6 @@ void FileHandler::division(int offset) {
 
 	while (!in.eof()) {
 		for (int i = 0; i < offset && std::getline(in, line); ++i) {
-
 			if (flagFirstLine != 1)
 				out1 << "\n";
 			else
@@ -69,7 +117,7 @@ void FileHandler::division(int offset) {
 	this->closeFiles(in, out1, out2);
 };
 
-void FileHandler::merge(int offset, int maxLength1, int maxLength2) {
+void FileHandler::merge(int offset) {
 	ofstream	out(fileInput);
 	ifstream	in1(fileOutput1), in2(fileOutput2);
 	string		line1, line2;
@@ -86,6 +134,7 @@ void FileHandler::merge(int offset, int maxLength1, int maxLength2) {
 				if (i > 0 || counter1 > 0 || counter2 > 0)
 					out << "\n";
 
+				++comps; ++moves;
 				if (this->compareLines(line1, line2)) {
 					++counter1;
 					out << line1;
@@ -102,21 +151,23 @@ void FileHandler::merge(int offset, int maxLength1, int maxLength2) {
 				}
 			}
 		}
-		
+
 		if (counter1 < offset) {
 			out << "\n" << line1;
 			++counter1;
-
+			++moves;
 			for (; counter1 < offset && std::getline(in1, line1); ++counter1) {
 				out << "\n" << line1;
+				++moves;
 			}
 		}
 		else {
 			out << "\n" << line2;
 			++counter2;
-
+			++moves;
 			for (; counter2 < offset && std::getline(in2, line2); ++counter2) {
 				out << "\n" << line2;
+				++moves;
 			}
 		}
 	}
@@ -124,20 +175,18 @@ void FileHandler::merge(int offset, int maxLength1, int maxLength2) {
 };
 
 void FileHandler::runSort() {
-	int current = 1;
-	while (current < length) {
+	for (int current = 1; current < length; current *= 2) {
 		this->division(current);
-		this->merge(current, current, current);
-		this->printFile();
-		current *= 2;
+		this->merge(current);
+		//this->printFile();
 	}
 };
 
 template<typename T1, typename T2, typename T3>
 void FileHandler::closeFiles(T1& file1, T2& file2, T3& file3) {
-		file1.close();
-		file2.close();
-		file3.close();
+	file1.close();
+	file2.close();
+	file3.close();
 };
 
 string FileHandler::getField(string line) {
@@ -155,7 +204,7 @@ bool FileHandler::compareLines(string line1, string line2) {
 	string requiredField1 = this->getField(line1);
 	string requiredField2 = this->getField(line2);
 	if (isdigit(requiredField1[0]))
-		return std::stoi(requiredField1) < std::stoi(requiredField2)
+		return std::stoi(requiredField1) <= std::stoi(requiredField2)
 		? true : false;
 	return requiredField1 < requiredField2
 		? true : false;
@@ -191,8 +240,8 @@ string FileHandler::generateLine() {
 void FileHandler::generateData() {
 	srand(static_cast<unsigned int>(time(0)));
 
-	std::cout << "Length of data = "; std::cin >> length;
-	std::cout << "Sort key = "; std::cin >> requiredNumber;
+	std::cout << "Number of lines = "; std::cin >> length;
+	//std::cout << "Sort key = "; std::cin >> requiredNumber;
 	ofstream output(fileInput);
 
 	for (int i = 0; i < length - 1; ++i)
@@ -226,68 +275,101 @@ void FileHandler::printFile() {
 	in.close();
 };
 
-void FileHandler::naturalDivision() {
+void FileHandler::naturalFirstDivision() {
 	ifstream	in(fileInput);
 	ofstream	out1(fileOutput1), out2(fileOutput2);
 	ofstream*	out = &out1;
 	string		line1, line2;
-	int			flagFirstLine = 1;
 	bool		flagWriting = true;
 
 	std::getline(in, line1);
-	//std::getline(in, line2);
+	line2 = line1;
+
 	while (!in.eof()) {
-		
-		while (std::getline(in, line2) && this->compareLines(line1, line2)) {
-			*out << line1 << "\n";
-			line1 = line2;
-			
-		}
-		*out << line1 << "\n\n";
 		line1 = line2;
-		
+		*out << line1 << "\n";
+		while (std::getline(in, line2) && ++comps && this->compareLines(line1, line2)) {
+			*out << line2 << "\n";
+			line1 = line2;
+		}
+		*out << "\n";
 		flagWriting = !flagWriting;
 		out = flagWriting ? &out1 : &out2;
-		
-		//while (std::getline(in, line2) && this->compareLines(line1, line2)) {
-		//	out2 << line1 << "\n";
-		//	line1 = line2;
-		//}
-		//out2 << line1 << "\n\n";
-		//line1 = line2;
 	}
 	if (line1 != line2)
 		*out << line2 << "\n\n";
-	//*out << line2 << "\n\n";
+		
 	this->closeFiles(in, out1, out2);
-	//delete out;
+};
+
+void FileHandler::naturalDivision() {
+	ifstream	in(fileInput);
+	ofstream	out1(fileOutput1), out2(fileOutput2);
+	ofstream*	out = &out1;
+	string		line;
+	bool		flagWriting = true;
+
+	for (;;) {
+		while (std::getline(in, line) && line != "") {
+			*out << line << "\n";
+		}
+		if (in.eof())
+			break;
+		*out << "\n";
+		flagWriting = !flagWriting;
+		out = flagWriting ? &out1 : &out2;
+	}
+
+	this->closeFiles(in, out1, out2);
 }
 
+void FileHandler::naturalMerge(int& amountOfSeries) {
+	ofstream	out(fileInput);
+	ifstream	in1(fileOutput1), in2(fileOutput2);
+	string		line1, line2;
+	int			sumCounter = 0;
 
-int main() {
-	string fileInput	= "D:\\forwork\\siaod5\\input.txt",
-		fileOutput1		= "D:\\forwork\\siaod5\\output1.txt",
-		fileOutput2		= "D:\\forwork\\siaod5\\output2.txt";
+	for (; sumCounter < length; ++amountOfSeries) {
+		std::getline(in1, line1);
+		std::getline(in2, line2);
 
-	
-	std::vector<std::vector<string>> fields = {
-		{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"}, 
-		{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"},
-		{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"},
-		{"18-70"},
-		{"150-210"},
-		{"green", "blue", "black", "brown"},
-		{"black", "blond", "brown", "grey"},
-		{"1000-1000000"},
-		{"yes", "no"},
-		{"yes", "no"}
-	};
-	FileHandler filehandler(fileInput, fileOutput1, fileOutput2, fields);
-	if (!filehandler.checkFiles())
-		return -1;
-	filehandler.generateData();
-	filehandler.naturalDivision();
-	//filehandler.runSort();
-	//filehandler.division(4);
-	return 0;
-}
+		for (; line1 != "" && line2 != ""; ++sumCounter) {
+			++comps; ++moves;
+			if (this->compareLines(line1, line2)) {
+				out << line1;
+				std::getline(in1, line1);
+			}
+			else {
+				out << line2;
+				std::getline(in2, line2);
+			}
+			out << "\n";
+		}
+		for (; line1 != ""; ++sumCounter) {
+			++moves;
+			out << line1 << "\n";
+			std::getline(in1, line1);
+		}
+		for (; line2 != ""; ++sumCounter) {
+			++moves;
+			out << line2 << "\n";
+			std::getline(in2, line2);
+		}
+		out << "\n";
+	}
+	this->closeFiles(out, in1, in2);
+};
+
+void FileHandler::runNaturalSort() {
+	int amountOfSeries;
+	this->naturalFirstDivision();
+
+	for (;;) {
+		amountOfSeries = 0;
+		this->naturalMerge(amountOfSeries);
+		if (amountOfSeries == 1)
+			break;
+		this->naturalDivision();
+		//this->printFile();
+	}
+};
