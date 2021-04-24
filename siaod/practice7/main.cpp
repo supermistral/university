f22d9c78ed2d0c8efa33202ca5c22a2fe9fba8ee
Michael <53310165+supermistral@users.noreply.github.com>
2021-04-25 02:37:21 +0300

    Update main.cpp

--- siaod/practice7/main.cpp
+++ siaod/practice7/main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include "node.h"
 
-void fillWithKnoweldgeAreas(List& list, ListKnowledgeArea*& listArea, int size) {
-    std::cout << "Number of areas = " << size << "\n";
-    for (Node* temp = list.head, int i = 0; temp; temp = temp->next, ++i) {
-        std::string line;
-        std::cout << i << ": ";
-        std::cin >> line;
-        while (line != "0") {
-            pushBack<NodeKnowledgeArea, ListKnowledgeArea, std::string>(
-                temp->data.knowledgeArea, line
-            );
-            std::cin >> line;
-        }
-    }
-}
+//void fillWithKnoweldgeAreas(List*& list, ListKnowledgeArea**& listArea, int size) {
+//    std::cout << "Number of areas = " << size << "\n";
+//    int ind = 0;
+//    for (Node* temp = list->head; temp; temp = temp->next, ++ind) {
+//        std::string line;
+//        std::cout << ind << ": ";
+//        std::cin >> line;
+//        listArea[ind] = new ListKnowledgeArea;
+//        while (line != "0") {
+//            pushBack<NodeKnowledgeArea, ListKnowledgeArea, std::string>(
+//                temp->data.knowledgeArea, line
+//            );
+//            std::cin >> line;
+//        }
+//    }
+//}
 
 
 int main()
 {
-    int size;
-    List list;
-    ListKnowledgeArea listKnowledgeArea;
-    ListKnowledgeArea* listKnowledgeAreaForList;
+    int size = 0;
+    List* list = nullptr;
+    ListKnowledgeArea* listKnowledgeArea = nullptr;
     std::string commands[] = {
         "create",
         "create knowledge",
@@ -38,37 +39,46 @@ int main()
     };
     std::string line;
     std::getline(std::cin, line);
-    while (line != "exit") {
-        if (line == commands[0])
-            list = createList(size);
-        else if (line == commands[1])
-            fillWithKnoweldgeAreas(list, listKnowledgeAreaForList, size);
-        else if (line == commands[2])
-            printFromLeftToRight(list.head);
-        else if (line == commands[3])
-            printFromRightToLeft(list.last);
-        else if (line == commands[4]) {
-            for (int i = 0; i < size; ++i) {
-                std::cout << i << ": ";
-                printFromLeftToRight(listKnowledgeAreaForList[0].head);
-            }
-        }
-        else if (line == commands[5]) {
-            for (int i = 0; i < size; ++i) {
-                std::cout << i << ": ";
-                printFromRightToLeft(listKnowledgeAreaForList[0].head);
-            }
-        }
-        else if (line == commands[6]) {
 
+    while (line != "exit") {
+        if (line == "create") {
+            list = createList(size);
         }
-        else if (line == commands[7]) {
+        else if (line == "create knowledge") {}
+            //fillWithKnoweldgeAreas(list, listKnowledgeAreaForList, size);
+        else if (line == "print")
+            printFromLeftToRight(list->head);
+        else if (line == "print reverse")
+            printFromRightToLeft(list->last);
+        else if (line == "print knowledge") {
+            for (int i = 0; i < size; ++i) {
+                std::cout << i << ": ";
+                //printFromLeftToRight<NodeKnowledgeArea>(listKnowledgeAreaForList[0]->head);
+            }
+        }
+        else if (line == "print knowledge reverse") {
+            for (int i = 0; i < size; ++i) {
+                std::cout << i << ": ";
+                //printFromRightToLeft<NodeKnowledgeArea>(listKnowledgeAreaForList[0]->head);
+            }
+        }
+        else if (line == "search") {
+            BookData dataToSearch;
+            std::cout << "Search: "; std::cin >> dataToSearch;
+            Node* node = searchNode(list->head, dataToSearch);
+
+            if (node)
+                std::cout << "Node was found -> " << node << "\n";
+            else
+                std::cout << "Node wasn't found\n";
+        }
+        else if (line == "push back") {
             BookData data;
-            ListKnowledgeArea newList;
-            std::cout << "New book data: ";
-            std::cin >> data;
-            std::cout << "New knowledge areas: ";
+            ListKnowledgeArea* newList = new ListKnowledgeArea;
             std::string line;
+            std::cout << "New book data: "; std::cin >> data;
+            std::cout << "New knowledge areas: "; 
+       
             std::cin >> line;
             while (line != "0") {
                 pushBack<NodeKnowledgeArea, ListKnowledgeArea, std::string>(
@@ -76,17 +86,32 @@ int main()
                 );
                 std::cin >> line;
             }
+
             data.knowledgeArea = newList;
             pushBack(list, data);
         }
-        else if (line == commands[8]) {
+        else if (line == "create knowledge area") {
+            listKnowledgeArea = createListKnowledgeArea(list->head);
+            printFromLeftToRight<NodeKnowledgeArea>(listKnowledgeArea->head);
+        }
+        else if (line == "remove") {
 
         }
-        std::cout << "done!\n\n";
+        else if (line == "move") {
+            int id;
+            std::cout << "Id = "; std::cin >> id;
+            moveNodes(*list, id);
+            printFromLeftToRight(list->head);
+        }
+        std::cout << "\n";
         std::getline(std::cin, line);
     }
     
-    delete[] listKnowledgeAreaForList;
+    delete list;
+    delete listKnowledgeArea;
+    //for (int i = 0; i < size; ++i)
+    //    delete[] listKnowledgeAreaForList[i];
+    //delete[] listKnowledgeAreaForList;
 
     return 0;
 }
