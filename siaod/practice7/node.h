878161be5f7ed00a8dd6d1237970a544394ac9bf
Michael <53310165+supermistral@users.noreply.github.com>
2021-04-25 22:57:32 +0300

    Update node.h

--- siaod/practice7/node.h
+++ siaod/practice7/node.h
@@ -1,3 +1,5 @@
+// node.h
+
 #pragma once
 #include <string>
 #include <iostream>
@@ -7,9 +9,9 @@ struct ListKnowledgeArea;
 struct BookData {
 	int number, year;
 	std::string author, name;
-	ListKnowledgeArea* knowledgeArea = nullptr;
+	ListKnowledgeArea* knowledgeArea;
 
-	~BookData();
+	BookData();
 };
 
 template <class T1, class T2>
@@ -22,23 +24,13 @@ struct NodeBase {
 struct Node
 	: NodeBase<Node, BookData> {
 
-	Node(Node* next, Node* prev, BookData data) {
-		this->next = next;
-		this->prev = prev;
-		this->data = data;
-	};
+	Node(Node*, Node*, BookData);
 };
 
 struct NodeKnowledgeArea
 	: NodeBase<NodeKnowledgeArea, std::string> {
 
-	NodeKnowledgeArea(
-		NodeKnowledgeArea* next, NodeKnowledgeArea* prev, std::string data
-	) {
-		this->next = next;
-		this->prev = prev;
-		this->data = data;
-	};
+	NodeKnowledgeArea(NodeKnowledgeArea*, NodeKnowledgeArea*, std::string);
 };
 
 template <class T>
@@ -59,25 +51,24 @@ struct ListKnowledgeArea
 	~ListKnowledgeArea();
 };
 
-
 std::ostream& operator<< (std::ostream&, const BookData&);
 std::istream& operator>> (std::istream&, BookData&);
-bool operator== (const BookData&, const BookData&);
 
-Node* searchNode(Node*, BookData);
-
+Node* searchNode(Node*, int);
 ListKnowledgeArea* createListKnowledgeArea(Node*);
-void removeNodes(Node*, std::string, std::string);
-void moveNodes(List&, int);
-void deleteNode(Node*&);
+void removeNodes(List*&, std::string, int);
+void moveNodes(List*&, int);
+void deleteNode(List*&, Node*&);
 List* createList(int&);
-
+void fillWithKnoweldgeAreas(Node*&);
+void printKnowledgeAreasFromLeftToRight(Node*&);
+void printKnowledgeAreasFromRightToLeft(Node*&);
 
 template <class T = Node>
 void printFromLeftToRight(T* head) {
 	std::cout << "List: " << head->data;
 	for (T* temp = head->next; temp; temp = temp->next)
-		std::cout << " -> " << temp->data;
+		std::cout << " <-> " << temp->data;
 	std::cout << "\n";
 }
 
@@ -85,7 +76,7 @@ template <class T = Node>
 void printFromRightToLeft(T* last) {
 	std::cout << "List: " << last->data;
 	for (T* temp = last->prev; temp; temp = temp->prev)
-		std::cout << " -> " << temp->data;
+		std::cout << " <-> " << temp->data;
 	std::cout << "\n";
 }
 
